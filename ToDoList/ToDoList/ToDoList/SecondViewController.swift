e9b5455007916da3bc03704f7128ba37f97542b1
Oyxiaoxi <Oyxiaoxi@Oyxiaoxi-MacBook-Pro.local>
2017-09-06 17:30:33 +0800

    This is Swift App. but some the guly !

---
+++ ToDoList/ToDoList/ToDoList/SecondViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by 欧阳晓锡 on 2017/9/6.
+//  Copyright © 2017年 Oyxiaoxi. All rights reserved.
+//
+
+import UIKit
+
+var list = ["Buy milk","Run 5 miles","Get Peter","Plant my new plants"]
+
+class SecondViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return (list.count)
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        myTableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
